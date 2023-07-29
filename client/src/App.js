992bcba35e6024643647ff8905e8a5a39c67b93f
jhorn00 <dawshorn@gmail.com>
2023-07-29 18:18:10 -0400

    Added deck.gl, react-map-gl, and a test dataset. Access works with ignored token env variable.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import NATIONAL_PARKS_DATA from "./data/test_data.json";
+import { Map } from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_MAPBOX_ACCESS_TOKEN;
+const MAP_STYLE = "mapbox://styles/mapbox/streets-v12";
+// const MAP_STYLE = "mapbox://styles/mapbox/satellite-streets-v12";
+const INITIAL_VIEW_STATE = {
+  latitude: 37.773972,
+  longitude: -122.431297,
+  zoom: 12,
+  bearing: 0,
+  pitch: 0
+};
 
 function App() {
+
+  const onClick = info => {
+    if(info.object) {
+      alert(info.object.properties.Name);
+    }
+  }
+
+  const layers = [
+    new GeoJsonLayer({
+      id: "nationalParks",
+      data: NATIONAL_PARKS_DATA,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 5,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 5,
+      getFillColor: data => data.properties.Name.includes("National Park") ? [0, 0, 0, 100] : [86, 144, 58, 100],
+      pickable:true,
+      autoHighlight: true,
+      onClick
+    })
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={layers}
+      >
+        <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN}>
+
+        </Map>
+      </DeckGL>
   );
 }
 
