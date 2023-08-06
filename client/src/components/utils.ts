5b15bfef8713be9c090e9654e48b5ef81741d104
jhorn00 <dawshorn@gmail.com>
2023-08-06 14:57:03 -0400

    Categorize colors some more.

--- client/src/components/utils.ts
+++ client/src/components/utils.ts
@@ -91,50 +91,91 @@ export function populateIncidentList(data: any[]): IncidentType[] {
 export function getColorCode(category: string): [number, number, number, number] {
     const categoryLower = category.toLowerCase();
     switch (categoryLower) {
+        // Violent Crimes
         case "assault":
-            return [192, 57, 43, 250]; // Dark red for Assault
-        case "case closure":
-            return [44, 62, 80, 250]; // Dark blue for Case Closure
-        case "disorderly conduct":
-            return [243, 156, 18, 250]; // Orange for Disorderly Conduct
-        case "drug violation":
-            return [241, 196, 15, 250]; // Yellow for Drug Violation
-        case "fraud":
-            return [230, 126, 34, 250]; // Orange for Fraud
-        case "gambling":
-            return [155, 89, 182, 250]; // Purple for Gambling
-        case "liquor laws":
-            return [46, 204, 113, 250]; // Green for Liquor Laws
-        case "larceny theft":
-            return [52, 152, 219, 250]; // Blue for Larceny Theft
-        case "lost property":
-            return [149, 165, 166, 250]; // Gray for Lost Property
-        case "malicious mischief":
-            return [211, 84, 0, 250]; // Red-orange for Malicious Mischief
-        case "missing person":
-            return [189, 195, 199, 250]; // Light gray for Missing Person
-        case "motor vehicle theft":
-            return [26, 188, 156, 250]; // Teal for Motor Vehicle Theft
-        case "non-criminal":
-            return [155, 89, 182, 250]; // Purple for Non-Criminal
-        case "prostitution":
-            return [155, 89, 182, 250]; // Purple for Prostitution
+        case "homicide":
         case "rape":
-            return [142, 68, 173, 250]; // Dark purple for Rape
-        case "recovered vehicle":
-            return [46, 204, 113, 250]; // Green for Recovered Vehicle
         case "robbery":
-            return [211, 84, 0, 250]; // Red-orange for Robbery
-        case "sex offense":
-            return [192, 57, 43, 250]; // Dark red for Sex Offense
+            return [211, 84, 0, 250]; // Red-orange
+
+        // Property Crimes
+        case "arson":
+        case "burglary":
+        case "larceny theft":
+        case "motor vehicle theft":
+        case "motor vehicle theft?":
         case "stolen property":
-            return [155, 89, 182, 250]; // Purple for Stolen Property
-        case "traffic collision":
-            return [231, 76, 60, 250]; // Dark red for Traffic Collision
+        case "vandalism":
+        case "vehicle misplaced":
+            return [52, 152, 219, 250]; // Blue
+
+        // White-Collar Crimes
+        case "embezzlement":
+        case "forgery and counterfeiting":
+        case "fraud":
+            return [200, 150, 11, 250]; // Gold
+
+        // Drug-Related Crimes
+        case "drug violation":
+        case "drug offense":
+            return [128, 0, 128, 250]; // Dark Purple
+
+        // Public Order Crimes
+        case "disorderly conduct":
+        case "gambling":
+        case "liquor laws":
+        case "prostitution":
+            return [155, 89, 182, 250]; // Purple
+
+        // Domestic Crimes
+        case "offences against the family and children":
+        case "sex offense":
+            return [192, 57, 43, 250]; // Dark red
+
+        case "human trafficking (a), commercial sex acts":
+        case "human trafficking, commercial sex acts":
+            return [255, 0, 0, 250];
+
+        // Miscellaneous Crimes
+        case "civil sidewalks":
+        case "courtesy report":
+        case "fire report":
+        case "miscellaneous investigation":
+        case "missing person":
+        case "non-criminal":
+        case "other":
+        case "other miscellaneous":
+        case "other offenses":
+        case "suicide":
+        case "suspicious":
+        case "suspicious occ":
+        case "traffic violation arrest":
+        case "unknown":
+        case "weapons carrying etc":
+        case "weapons offense":
+        case "weapons offence":
+            return [192, 57, 43, 250]; // Dark red
+
+        // Other Categories
+        case "case closure":
+            return [44, 62, 80, 250]; // Dark blue
+
+        case "lost property":
+            return [149, 165, 166, 250]; // Gray
+
+        case "malicious mischief":
+            return [211, 84, 0, 250]; // Red-orange
+
+        case "recovered vehicle":
         case "vehicle impounded":
-            return [46, 204, 113, 250]; // Green for Vehicle Impounded
+            return [46, 204, 113, 250]; // Green
+
+        case "traffic collision":
+            return [231, 76, 60, 250]; // Dark red
+
         case "warrant":
-            return [52, 152, 219, 250]; // Blue for Warrant
+            return [52, 152, 219, 250]; // Blue
+
         default:
             return [149, 165, 166, 250]; // Default color for other categories (Gray)
     }
